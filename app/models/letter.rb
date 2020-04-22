d408ed11d0058f0469780e48a803181204e7e52e
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2020-04-22 21:16:22 +0700

    add_model_letter_and_product

---
+++ app/models/letter.rb
@@ -0,0 +1,25 @@
+class Letter
+  class << self
+    def all
+      ("a".."z").map {|char| Letter.new(char)}
+    end
+
+    def find_by params
+      all.detect {|letter| letter.char == params[:char]&.downcase } || raise(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  attr_reader :char
+
+  def initialize char
+    @char = char
+  end
+
+  def to_params
+    char
+  end
+
+  def products
+    Product.where("name LIKE ?", "#{char}%")
+  end
+end
